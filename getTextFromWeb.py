80b731b28ebf127f43da8572caa28733e55e1370
dage <allen_zhang99@126.com>
2025-01-10 22:48:36 +0800

    对项目的结构进行了优化使其更加清晰。

--- getTextFromWeb.py
+++ getTextFromWeb.py
@@ -1,13 +1,18 @@
+import os
+
 import requests
 from bs4 import BeautifulSoup
 
-zhangjie_number = 156997
-page_number = 2
-is_first_page = True
-is_last_page = False
-last_zhangjie_number = 157001
+novel_title = "樱花庄的宠物女孩第10.5卷"
 juan_number = "第10.5卷"
 global_text_title = "第10.5卷 长谷栞奈突如其来的教育旅行"
+novel_code = 2081
+first_zhangjie_number = 156997
+page_number = 2
+last_zhangjie_number = 157001
+zhangjie_change_interval = 1
+is_first_page = True
+is_last_page = False
 has_text_title = True
 is_retying = False
 is_first_page_connected_success = True
@@ -18,13 +23,23 @@ headers = {
 }
 
 # 定义需要删除的特殊字符和特定文本
-special_char = '  '
+special_char = ' '
 failed_urls = {}
 max_attempts = 3
 #让一章的总标题在一章内只会在这一章的开头出现一次。
 specific_text = ['上一章点赞目录+书签下一页','上一章点赞目录+书签下一章','上一页点赞目录+书签下一章',
                 '上一页点赞目录+书签下一页','轻之文库轻小说-懂阅读更懂创作的轻小说平台-发现和创造有趣的故事-日本动漫轻小说在线阅读',
                 '当前位置:轻之文库轻小说-懂阅读更懂创作的轻小说平台-',juan_number]
+
+def write_result_to_file(result, text_path):
+    if not os.path.exists(f"小说\\{novel_title}"):
+        os.makedirs(f"小说\\{novel_title}")
+    with open(text_path, 'a', encoding='utf-8') as f:
+        f.write(result)
+        if is_first_page:
+            print(f"{global_text_title} 第{first_zhangjie_number} 章 第1 页 已保存")
+        else:
+            print(f"{global_text_title} 第{first_zhangjie_number} 章 第{page_number} 页 已保存")
 
 def is_special_text_in_line(line, specific_texts):
     for specific_text1 in specific_texts:
@@ -95,13 +110,8 @@ def get_text_from_web(urls):
 
         # 打印或处理获取到的文字内容
         #print(cleaned_text)
-        text_path = f'back\\小说\\{global_text_title}{zhangjie_number}章1.txt' if is_first_page else f'back\\小说\\{global_text_title}{zhangjie_number}章{page_number}.txt'
-        with open(text_path, 'a', encoding='utf-8') as f:
-            f.write(cleaned_text)
-            if is_first_page:
-                print(f"{global_text_title} 第{zhangjie_number} 章 第1 页 已保存")
-            else:
-                print(f"{global_text_title} 第{zhangjie_number} 章 第{page_number} 页 已保存")
+        text_path = f'小说\\{novel_title}\\{global_text_title}{first_zhangjie_number}章1.txt' if is_first_page else f'小说\\{novel_title}\\{global_text_title}{first_zhangjie_number}章{page_number}.txt'
+        write_result_to_file(cleaned_text, text_path)
         return
     else:
         print('Failed to retrieve the webpage')
@@ -121,16 +131,23 @@ def start_requests(current_url,attempts=0):
             attempts += 1
 
     if attempts == max_attempts:
-        failed_urls[current_url] = (zhangjie_number,page_number)
+        failed_urls[current_url] = (first_zhangjie_number, page_number)
         return False
     return True
 
-if __name__ == '__main__':
+def download_novel():
+    global is_first_page
+    global is_last_page
+    global is_first_page_connected_success
+    global first_zhangjie_number
+    global page_number
+    global last_zhangjie_number
+    global zhangjie_change_interval
 
     while True:
         # 目标网站的URL
-        first_page_url = f'https://www.wenkuchina.com/lightnovel/2081/{zhangjie_number}.html'
-        url = f'https://www.wenkuchina.com/lightnovel/2081/{zhangjie_number}_{page_number}.html'
+        first_page_url = f'https://www.wenkuchina.com/lightnovel/{novel_code}/{first_zhangjie_number}.html'
+        url = f'https://www.wenkuchina.com/lightnovel/{novel_code}/{first_zhangjie_number}_{page_number}.html'
         if is_first_page:
             if not start_requests(first_page_url):
                 is_first_page_connected_success = False
@@ -143,21 +160,34 @@ if __name__ == '__main__':
                 start_requests(url)
                 page_number += 1
             else:
-                if zhangjie_number == last_zhangjie_number:
+                if first_zhangjie_number == last_zhangjie_number:
                     break
                 else:
+                    #切换到下一张时，重置页码号
                     page_number = 2
-                    text_piece = 1
-                    zhangjie_number += 1
+                    first_zhangjie_number += zhangjie_change_interval
                     is_last_page = False
                     is_first_page = True
+
+def retry_failed_urls():
+    global is_retying
+    global failed_urls
+    global first_zhangjie_number
+    global page_number
 
     while failed_urls:
         is_retying = True
         url_page_pair = failed_urls.popitem()
         failed_url = url_page_pair[0]
         page_number = url_page_pair[1][1]
-        zhangjie_number = url_page_pair[1][0]
+        first_zhangjie_number = url_page_pair[1][0]
         start_requests(failed_url)
         print(f"正重新尝试失败的链接... 剩余链接：{len(failed_urls)}")
+
+def main():
+    download_novel()
+    retry_failed_urls()
     print("\n" "Failed URLs:", failed_urls)
+
+if __name__ == '__main__':
+    main()
